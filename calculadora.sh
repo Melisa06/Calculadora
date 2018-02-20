6daac6c79866310a37ac48c46f9a27eb4c90f79f
Melisa06 <mel.avila0906@gmail.com>
2018-02-20 21:59:38 +0000

    Second commit

--- calculadora.sh
+++ calculadora.sh
@@ -15,10 +15,38 @@ echo "Tu resultado es: $resultado"
          ;;
 
  resta)  echo  "Haz elegido resta"
-               ;;  
+echo "Ingresa tu primer numero"
+read n1 
+ echo "Ingresa tu siguiente nùmero"
+read n2 
 
-Multiplicacion)  echo    "Haz elegido multiplicar"
+resultado_resta=$(($n1 -$n2))
+echo "Tu resultado de la resta es:  $resultado_resta"                
+
+;;  
+
+multiplicar)  echo    "Haz elegido multiplicar"
+echo  "Ingresa tu primer nùmero:"
+read n1 
+echo "ingresa el siguiente numero:"
+read n2
+
+resultado_multiplicar=$(($n1 * $n2))
+
+echo "Tu resultado es: $resultado_multiplicar"
+
                   ;;
+dividir) echo "Haz elegido dividir "
+echo  "Ingresa tu primer nùmero:"
+read n1 
+echo "Ingresa el siguiente nùmero:"
+read n2 
+
+resultado_dividir=$(($n1 / $n2))
+
+echo "Tu resultado es:  $resultado_dividir"
+;;
+
 
 
 *)    echo "No haz elegido ninguna operaciòn Bye!"
